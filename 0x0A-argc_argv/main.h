718aef2c8c126a537f3184ac2d775438d686499b
Tinuade Williams <williamstinuade02@gmail.com>
2023-03-10 15:46:06 -0800

    tasks

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,3 +1,5 @@
-int _putchar(char c);                                                                                                                
-int check_num(char *str);
-
+#ifndef MAIN_H	
+#define MAIN_H
+int _putchar(char c);
+int _atoi(char *s);		
+#endif
